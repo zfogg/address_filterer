54d071e9abd33e5a1a803eb0a9b3f2c9d056378a
Zachary Fogg <zach@zachfogg.com>
2011-10-06 18:24:06 -0500

    --dry-run

--- mail_list.py
+++ mail_list.py
@@ -7,106 +7,91 @@ THIS_DIR = os.path.split(THIS_FILE)[0]
 PARENT_DIR = os.path.split(THIS_DIR)[0]
 
 def main(files):
-
-	csv_data = []
-	keys = ()
-	for file in files:
-		print "\tTarget: %s\n" % file.split('\\')[-1]
-		data = [line for line in csv.reader( open(file) )]
-		keys = data.pop(0)
-		print keys
-		csv_data += data
-	raw_input()
-	total_to_parse = len(csv_data)
+	print "\n\t###\n"
+	csv_data = csv_from_files(files)
+	print "\n\n\tTotal addresses: %d\n" % len(csv_data)
+	csv_data = without_invalid(csv_data)
+	print "Without invalid: %d\n" % len(csv_data)
+	csv_data = without_duplicates(csv_data)
+	print "Without duplicates: %d\n" % len(csv_data)
+	write_data(csv_data)
 	
+def csv_from_files(files):
+	csv_data = []
+	for file in files:
+		print "Reading file: %s" % file.split("\\")[-1]
+		data = [clean_dict(dict) for dict in csv.DictReader(open(file))]
+		csv_data += data
+	return csv_data
+	
+def without_invalid(data):
 	print "Validating addresses . . ."
 	valid_keys = ('zip', 'city')
 	print "Validation keys: %s" % str(valid_keys)
-	dicts = [dict for dict in csv_to_dictionaries(csv_data, keys) if valid_dict(dict, valid_keys)]
-	total_filtered = total_to_parse - len(dicts)
-	print "%d valid addresses = %d total - %d invalid" % (
-		total_to_parse - total_filtered, total_to_parse, total_filtered
-	)
+	return [dict for dict in data if valid_dict(dict, valid_keys)]
 	
-	print "\n"
-	
+def without_duplicates(data):
 	print "Filtering duplicates . . ."
 	unique_keys = ('address1', 'zip')
 	print "Duplication signature: %s" % str(unique_keys)
-	results = f5_filter( [tuple(x.items()) for x in dicts], address_signature(unique_keys) )
-	duplicates_found = total_to_parse - total_filtered - len(results)
-	print "%d addresses = %d valid - %d duplicates" % (
-		len(results), total_to_parse - total_filtered, duplicates_found
-	)
-
-def csv_to_dictionaries(data, keys):
-	for line in data:
-		data_dictionary = {}
-		for i in range( len(keys) ):
-			data_dictionary[ keys[i] ] = clean_text(line[i], keys[i])
-		yield data_dictionary
+	return f5_filter( [x for x in data], address_signature(unique_keys) )
 		
+def write_data(data):
+	with open(PARENT_DIR + "\\results.csv", "wb") as f:
+		writer = csv.DictWriter(f, data[0].keys(), extrasaction='ignore')
+		writer.writeheader()
+		writer.writerows(data)
+
 def valid_dict(dict, valid_keys):
 	for k in valid_keys:
 		if not dict[k]: return False
-	country = dict['country']
-	if country != 'US' and country != 'USA' and country != '':
+		
+	c = dict['country']
+	if c != 'US' and c != 'USA' and c != '':
 		return False
-	if not dict['zip'].isdigit(): return False
+	if not dict['zip'].isdigit():
+		return False
 	
 	return True
 
 def address_signature(unique_keys):
 	def f(address):
 		signature = []
-		for element in address:
-			for uk in unique_keys:
-				if uk in element: signature.append(element)
+		for k, v in address.items():
+			if k in unique_keys:
+				signature.append(v)
 		return tuple(signature)
 	return f
 	
 def f5_filter(seq, idfun=lambda x: x):
-    seen = {}
-    result = []
-    for item in seq:
-        marker = idfun(item)
-        if marker in seen: continue
-        seen[marker] = 1
-        result.append(item)
-    return result
+	seen = {}
+	result = []
+	for item in seq:
+		marker = idfun(item)
+		if marker in seen: continue
+		seen[marker] = 1
+		result.append(item)
+	return result
 		
-def clean_text(text, key):
-	text = text.replace('.', '').upper().strip(" ")
-	
-	if key == 'address1':
-		if text.endswith(' ST'):
-			text = text[:-2] + 'STREET'
-		elif text.endswith(' RD'):
-			text = text[:-2] + "ROAD"
-		elif text.endswith(' AV') or text.endswith(" AVE"):
-			text = ' '.join(text.split(' ')[:-1]) + " AVENUE"
-		elif text.endswith(' PL'):
-			text = text[:-2] + "PLACE"
-
-	if key == 'zip':
-		text = text.split('-')[0]
-
-	return text
+def clean_dict(dict):
+	def clean(text, key):
+		text = text.replace('.', '').replace("  ", ' ').upper().strip(" ")
+		if key == 'address1':
+			if text.endswith(' ST'):
+				text = text[:-2] + 'STREET'
+			elif text.endswith(' RD'):
+				text = text[:-2] + "ROAD"
+			elif text.endswith(' AV') or text.endswith(" AVE"):
+				text = ' '.join(text.split(' ')[:-1]) + " AVENUE"
+			elif text.endswith(' PL'):
+				text = text[:-2] + "PLACE"
+		if key == 'zip':
+			text = text.split('-')[0]
+		return text
 		
-def log_csv_tuples(csv_tuples):
-	i = 0
-	for tup in csv_tuples:
-		print "\n%d" % i
-		log_tuple(tup)
-		i += 1
-	
-def log_tuple(tup):
-	for k, v in tup: print "\t%s : %s" % (k, v)
+	for k, v in dict.items():
+		dict[k] = clean(dict[k], k)
+	return dict
 	
 if __name__ == "__main__":
-	data_path = PARENT_DIR + "\\data\\"
-	files = glob( os.path.join(data_path, '*.csv') )
-	
-	#file = PARENT_DIR + "\\data\\" + "quickbooks_all.csv"
-	main(files)
-	#else: print "No such file."
+	main( glob( os.path.join( PARENT_DIR + "\\data\\", '*.csv' ) ) )
