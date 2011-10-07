e24aa8c8dabf11a5fa08d7dda816681742032f94
Zachary Fogg <zach@zachfogg.com>
2011-10-07 13:50:14 -0500

    Renamed some functions and arguments to more logical names that describe their purposes better. clean_text is now produced from a closure that maps a function over a dictionary in a specific way. It actually modifies the copy of the dictionary that is passed in currently, but perhaps it should leave the original untouched and return a modified duplicate. The output file now has headers written as the first line.
    dict_signaturizer(signature_keys) is a closure that returns a function that will create a signature for any dictionary. A signature is defined as a tuple of the values contained in the dict at each key in signature_keys. For my purposes, a tuple of the cleaned 'address1' and 'zip' values will be sufficient to filter out most any duplicate entry.
    
    duplicate_filter(collection, signature) has had its values renamed to better signify their purpose.

--- mail_list.py
+++ mail_list.py
@@ -13,10 +13,10 @@ def main(files):
 	csv_data = csv_from_files(files)
 	print "\n\n\tTotal addresses: %d\n" % len(csv_data)
 	
-	csv_data = without_invalid(csv_data)
+	csv_data = without_invalid(csv_data, ('zip', 'city'))
 	print "Without invalid: %d\n" % len(csv_data)
 	
-	csv_data = without_duplicates(csv_data)
+	csv_data = without_duplicates(csv_data, ('address1', 'zip'))
 	print "Without duplicates: %d\n" % len(csv_data)
 	
 	write_data(csv_data)
@@ -24,34 +24,65 @@ def main(files):
 	
 def csv_from_files(files):
 	csv_data = []
+	clean_dict = dict_kv_mapper(clean_text)
 	for file in files:
 		print "Reading file: %s" % file.split("\\")[-1]
-		data = [clean_dict(dict) for dict in csv.DictReader(open(file))]
+		data = [clean_dict(dict) for dict in csv.DictReader( open(file) )]
 		csv_data += data
+		
 	return csv_data
 	
-def without_invalid(data):
+def without_invalid(data, validation_keys):
 	print "Validating addresses . . ."
-	valid_keys = ('zip', 'city')
-	print "Validation keys: %s" % str(valid_keys)
-	return [dict for dict in data if valid_dict(dict, valid_keys)]
+	print "Validation keys: %s" % str(validation_keys)
 	
-def without_duplicates(data):
+	return [dict for dict in data if valid_dict(dict, validation_keys)]
+	
+def without_duplicates(data, duplication_keys):
 	print "Filtering duplicates . . ."
-	unique_keys = ('address1', 'zip')
-	print "Duplication signature: %s" % str(unique_keys)
-	return duplicate_filter( [x for x in data], address_signature(unique_keys) )
+	print "Duplication signature: %s" % str(duplication_keys)
+	
+	return duplicate_filter(data, dict_signaturizer(duplication_keys))
 		
 def write_data(data, outfile=OUTFILE):
-	with open(outfile, "wb") as f:
-		writer = csv.DictWriter(f, data[0].keys(), extrasaction='ignore')
+	with open(outfile, "wb") as file:
+		writer = csv.DictWriter(file, data[0].keys(), extrasaction='ignore')
 		writer.writeheader()
 		writer.writerows(data)
+		
+def dict_kv_mapper(f):
+	def kv_map(dict):
+		for k, v in dict.items():
+			dict[k] = f(k, v)
+		return dict
+			
+	return kv_map
+	
+def clean_text(key, text):
+	text = text.replace('.', '').replace("  ", ' ').upper().strip(" ")
+	
+	if key == 'address1':
+		if text.endswith(' ST'):
+			text = text[:-2] + 'STREET'
+		elif text.endswith(' RD'):
+			text = text[:-2] + "ROAD"
+		elif text.endswith(' AV') or text.endswith(" AVE"):
+			text = ' '.join(text.split(' ')[:-1]) + " AVENUE"
+		elif text.endswith(' PL'):
+			text = text[:-2] + "PLACE"
+		elif text.endswith(' WY'):
+			text = text[:-2] + "WAY"
+			
+	if key == 'zip':
+		text = text.split('-')[0]
+		
+	return text
 
 def valid_dict(dict, valid_keys):
 	for k in valid_keys:
 		if not dict[k]:
 			return False
+			
 	c = dict['country']
 	if c != 'US' and c != 'USA' and c != '':
 		return False
@@ -59,45 +90,25 @@ def valid_dict(dict, valid_keys):
 		return False
 	
 	return True
-
-def address_signature(unique_keys):
-	def f(address):
-		signature = []
-		for k, v in address.items():
-			if k in unique_keys:
-				signature.append(v)
-		return tuple(signature)
-	return f
 	
-def duplicate_filter(set, idfun=lambda x: x): # Credit: Alex Martelli - http://www.peterbe.com/plog/uniqifiers-benchmark
-    seen = {}
-    result = []
-    for item in set:
-        marker = idfun(item)
-        if marker not in seen:
-            seen[marker] = 1
-            result.append(item)
-    return result
+def duplicate_filter(collection, signaturize=lambda x: x):
+	# Credit: Alex Martelli - http://www.peterbe.com/plog/uniqifiers-benchmark
+    signatures = {}
+    results = []
+    for item in collection:
+        sig = signaturize(item)
+        if sig not in signatures:
+            signatures[sig] = True
+            results.append(item)
+			
+    return results
+
+def dict_signaturizer(signature_keys):
+	def signature(dict):
+		return tuple([v for k, v in dict.items() if k in signature_keys])
 		
-def clean_dict(dict):
-	def clean(text, key):
-		text = text.replace('.', '').replace("  ", ' ').upper().strip(" ")
-		if key == 'address1':
-			if text.endswith(' ST'):
-				text = text[:-2] + 'STREET'
-			elif text.endswith(' RD'):
-				text = text[:-2] + "ROAD"
-			elif text.endswith(' AV') or text.endswith(" AVE"):
-				text = ' '.join(text.split(' ')[:-1]) + " AVENUE"
-			elif text.endswith(' PL'):
-				text = text[:-2] + "PLACE"
-		if key == 'zip':
-			text = text.split('-')[0]
-		return text
-		
-	for k, v in dict.items():
-		dict[k] = clean(dict[k], k)
-	return dict
+	return signature
+	
 	
 if __name__ == "__main__":
 	main( glob( os.path.join( PARENT_DIR + "\\data\\", '*.csv' ) ) )
